374aedd49d3e2b7ff4e17468be91e05438527c59
Christophe de Dinechin <christophe@taodyne.com>
2013-06-07 22:51:59 +0200

    Put priority of 'with' where it belongs, fixes #3043
    The problem arose in expressions such as 'foo with -10..10'.
    The change for #3006 makes us return to statement mode for the - in -10,
    so that we parse that as -(10..10) and not -10..10.
    
    In order to avoid this, we need 'with' to mark an expression, which actually
    corresponds to reality (i.e. foo with 1..5 is an expression returning a list).

--- doc/film_strip.doxy.h
+++ doc/film_strip.doxy.h
@@ -5,8 +5,7 @@
  * Show video or other content in the frames of a 35mm film strip.
  * This module also provides a GLSL shader to simulate defects commonly
  * found on old movies (color fading, lighting instabilities, scratches
- * and splotches), as well as as movie countdown animation that looks like
- * the sequence commonly used for 35mm movies.
+ * and splotches).
  *
  * Here is an example (<a href="hello_strip.ddd.txt">hello_strip.ddd</a>)
  * that re-uses the "Hello World" demo, bundled with
@@ -28,8 +27,7 @@
  * pellicule 35mm. Ce module fournit également un shader GLSL qui simule
  * les effets du vieillissement des films photographiques (couleurs délavées,
  * scintillements, rayures et taches), et qui peut être appliqué à des vidéos
- * ou tout autre contenu. Également disponible : un compte à rebours animé qui
- * ressemble à la séquence communément utilisée au début des films 35mm.
+ * ou tout autre contenu.
  *
  * Voici un exemple (<a href="hello_strip.ddd.txt">hello_strip.ddd</a>)
  * qui réutilise la démonstration "Hello World" fournie
@@ -127,94 +125,6 @@ old_movie_shader();
  * @endcode
  */
 old_movie_shader_amount(Amount:real);
-
-
-/**
- * @~english
- * Show an animated countdown as found at the beginning of some movies.
- *
- * The animation looks like the "SMPTE Universal Leader", commonly used from
- * the mid-60s for television and theatrical projection applications.
- * This effect is not meant to be a precise reproduction of the original test
- * sequence. Its purpose is just to suggest that a movie is about to begin.
- *
- * With the default parameters, the countdown goes from 8 to 2, each number
- * being shown during 1 second except number 2, which is shown for a very short
- * amount of time and followed by 2 seconds of black.
- *
- * The @ref FLNoiseAmount variable may be used to add some video noise.
- * @ref FLPopVolume controls the audio volume of the quick beep that is
- * normally heard when number "2" is shown.
- *
- * Example (<a href="countdown.ddd.txt">countdown.ddd</a>):
- * @~french
- * Affiche un décompte animé similaire à celui utilisé au cinéma.
- *
- * L'animation ressemble à la séquence "SMPTE Universal Leader", couramment
- * utilisée à partir du milieu des années 60 à la télévision et dans les salles
- * de projection.
- * Cet effet ne prétend pas être une reproduction précise de la séquence
- * originale ; il est simplement fait pour suggérer qu'un film va commencer.
- *
- * Avec les paramètres par défaut, le décompte va de 8 à 2. Chaque chiffre
- * est affiché pendant une seconde, sauf le 2 qui apparaît durant un temps
- * très court et est suivi de deux secondes de noir.
- *
- * @p Start est la valeur de page_time à laquelle l'animation commence, par
- * défaut : 0.0.
- * @p Duration est la durée totale de l'animation, par défaut : 8.0.
- * Enfin, @p W et @p H sont la largeur et la hauteur du rectangle qui affiche
- * l'effet, par défaut : window_width et window_height, respectivement.
- *
- * Certains aspects sont configurables par des variables, avant d'appeler
- * l'effet.
- * La variable @ref FLNoiseAmount permet de superposer du bruit vidéo (de la
- * "neige"). Quant à @ref FLPopVolume, elle contrôle le volume audio du bip
- * qui est normalement émis lorsque le chiffre "2" est affiché.
- *
- * Exemple (<a href="countdown.ddd.txt">countdown.ddd</a>):
- * @~
- * @include "countdown.ddd.txt"
- * @since 1.001 (Tao 1.31)
- */
-film_leader_countdown(Start:real, Duration:real, W:real, H:real);
-
-
-/**
- * @~english
- * The amount of video noise (static noise) in @ref film_leader_countdown.
- * 0.0 is "no noise", 1.0 is "medium noise", higher values increase the amount of
- * noise.
- *
- * @~french
- * Quantité de bruit de fond vidéo (neige) dans @ref film_leader_countdown.
- * 0.0 signifie "pas de bruit", 1.0 correspond à un niveau de bruit moyen,
- * toute valeur supérieure augmente le bruit.
- * @~
- * @since 1.001 (Tao 1.31)
- */
-FLNoiseAmount = 0.0;
-
-
-/**
- * @~english
- * The audio volume of the short "pop" played when @ref film_leader_countdown reaches 2.
- * Possible values range from 0.0 (no pop) to 1.0 (normal volume of the
- * sound sample).
- *
- * If you set FLPopVolume to a non-zero value, you <b>must</b> import the
- * <tt>VLCAudioVideo</tt> module.
- * @~french
- * Le niveau de volume audio du "bip" joué lorsque @ref film_leader_countdown affiche 2.
- * Les valeurs possibles vont de 0.0 (pas de bip) à 1.0 (volume normal de
- * l'échantillon sonore).
- *
- * Si vous donnez une valeur non nulle à FLPopVolume, vous <b>devez</b>
- * importer le module <tt>VLCAudioVideo</tt>.
- * @~
- * @since 1.001 (Tao 1.31)
- */
-FLPopVolume = 0.0;
 
 /**
  * @}
--- film_strip.pro
+++ film_strip.pro
@@ -16,10 +16,6 @@ include(../crypt_xl.pri)
 INSTALLS    += thismod_icon
 INSTALLS    -= thismod_bin
 
-inst_others.files = 2-pop.wav
-inst_others.path  = $${MODINSTPATH}
-QMAKE_TARGETS += inst_others
-INSTALLS += inst_others
 
 QMAKE_SUBSTITUTES = doc/Doxyfile.in
 QMAKE_DISTCLEAN = doc/Doxyfile
@@ -28,6 +24,5 @@ DOXYLANG = en,fr
 DOXYADDFILES = doc/hello_strip.ddd.txt \
                doc/old_movie.ddd.txt \
                doc/old_movie_fr.ddd.txt \
-               doc/old_video.ddd.txt \
-               doc/countdown.ddd.txt
+               doc/old_video.ddd.txt
 include(../modules_doc.pri)
